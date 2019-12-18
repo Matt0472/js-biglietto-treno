b85828b5151145e95c8f49a79a1b3a2e80a3fc75
Mattia Pedone <mattiapedone93@gmail.com>
2019-12-18 17:29:05 +0100

    inserimento primi prompt nel file JS

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole
+// percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+var passengersYearsOld = parseInt(prompt('Buongiorno! Quanti anni ha?'));
+var passengersKm = parseInt(prompt('Quanti km vorrebbe percorrere nella sua tratta?'));
